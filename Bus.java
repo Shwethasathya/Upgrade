645c88bf45a94178be29d69251ef3489741ef0c9
Shwethasathya <113284867+Shwethasathya@users.noreply.github.com>
2022-10-11 22:12:41 +0530

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,14 @@
+package com.xworkz.travel.rain;
+
+public class Bus {
+
+	public int number;
+	public String starting;
+	public String destination;
+	
+	public Bus()
+	{
+		System.out.println("executing constructor");
+	}
+	
+}
