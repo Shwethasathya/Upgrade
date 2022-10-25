d50919836a258e0e9049176edefa7778575a9ea9
sshwetha.xworkz@gmail.com <sshwetha.xworkz@gmail.com>
2022-10-25 16:20:53 +0530

    upload

---
+++ Flowername.java
@@ -0,0 +1,6 @@
+package com.xworkz.constance.cons;
+
+public enum Flowername {
+
+	ROSE, JASMINE, CRYSANTIMUM, KANAKAMBARA, TULASI;
+}
---
+++ ShopName.java
@@ -0,0 +1,7 @@
+package com.xworkz.constance.cons;
+
+public enum ShopName {
+
+	KANTI_SWEETS, VENKATESHWARA_SWEETS_NANDHINI, ASHA_SWEETS;
+
+}
